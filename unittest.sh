599bf4bf2ff2d2f5e6aba6a25167d434e19da43b
Ultrafeel <miketut@gmail.com>
2017-06-07 13:24:38 +0300

    unittest improve

--- unittest.sh
+++ unittest.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
 arg_class=kerncalc_arg-class
-echo + op 
+b=_________________
+
+fail =$b fail
+right=$b success 
+echo '+' op 
 echo 12 > /sys/class/$arg_class/argument1
 echo 23 > /sys/class/$arg_class/argument2
 echo + > /dev/operand
 res=`cat /dev/result`
 if [ "$res" -eq 35 ]
 then
-	echo right
+	echo $right
 else
-	echo wrong
+	echo $fail
 fi
 
-echo - op 
+echo '-' op 
 echo 1 > /sys/class/$arg_class/argument1
 echo 15 > /sys/class/$arg_class/argument2
 echo - > /dev/operand
 res=`cat /dev/result`
 if [ "$res" -eq -14 ]
 then
-	echo right
+	echo $right
 else
-	echo wrong
-fi
+	echo $fail
