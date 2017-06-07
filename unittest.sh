14d3c83b7871edf61974c80cf0e5ccf69b21dd3f
Ultrafeel <miketut@gmail.com>
2017-06-07 14:24:07 +0300

    Fix unittest.sh

--- unittest.sh
+++ unittest.sh
@@ -3,8 +3,8 @@
 arg_class=kerncalc_arg-class
 b=_________________
 
-fail =$b fail
-right=$b success 
+fail ="$b fail"
+right="$b success"
 echo '+' op 
 echo 12 > /sys/class/$arg_class/argument1
 echo 23 > /sys/class/$arg_class/argument2
@@ -27,3 +27,4 @@ then
 	echo $right
 else
 	echo $fail
+fi
